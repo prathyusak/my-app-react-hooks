5f17661137abab0fc7006af3816d9ecfa6899b4b
prathyusak <kandaprathyusha@gmail.com>
2020-11-09 19:17:58 -0700

    first commit

---
+++ build/precache-manifest.c682a5d75958af8791c8c10d4f6e94fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0fe43b0abaf2f0c060791d5072fa9d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ffa94fa46ef825496d86",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "d36d67fe1146f506f264",
+    "url": "/static/css/main.8d171f38.chunk.css"
+  },
+  {
+    "revision": "ffa94fa46ef825496d86",
+    "url": "/static/js/2.307e5030.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.307e5030.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d36d67fe1146f506f264",
+    "url": "/static/js/main.5ce1e8d5.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
